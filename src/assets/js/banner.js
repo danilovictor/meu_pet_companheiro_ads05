9d21169ca9e8db7d275a153bfc6c57197242a619
Danilo Victor <danilo-almeida12@hotmail.com>
2021-09-24 21:19:19 -0300

    imgqrcode

--- src/assets/js/banner.js
+++ src/assets/js/banner.js
@@ -15,5 +15,5 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Troca imagem em 3 segundos
+  setTimeout(showSlides, 3000); // Troca imagem em 3 segundos                                                                                                                                                                                                                                                                                                                                                                                                                                                              
 }
