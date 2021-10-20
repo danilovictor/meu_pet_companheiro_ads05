300657107df9c6f2679a2451dc5cc3a0bc58f5f3
Danilo Victor <danilo-almeida12@hotmail.com>
2021-10-20 19:12:17 -0300

    insercaoalert

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,1 +1,9 @@
+let exibirModal = document.getElementById('modaljs');
 
+
+exibirModal.addEventListener('click',mostrarAlert);
+
+
+function mostrarAlert(){
+     alert('seu pedido de adoção está sendo analisado')
+};
