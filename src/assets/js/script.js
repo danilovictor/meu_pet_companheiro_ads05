1ddfeae310873f38f27502aeca722a795da50034
Danilo Victor <danilo-almeida12@hotmail.com>
2021-10-26 21:00:11 -0300

    dasboardadmin

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -5,3 +5,28 @@ exibirModal.addEventListener('click',mostrarAlert);
 function mostrarAlert(){
      swal( " Seu pedido de adoção está em análise " , " Em breve entramos em contato com você via whatsapp ou E-mail! " , " warning " );
 };
+
+
+function login(){
+     var done = 0;
+     var usuario = document.getElementById('email')[0].value;
+     var senha = document.getElementById('senha')[0].value;
+
+     usuario = usuario.toLowerCase();
+     senha = senha.toLowerCase();
+
+
+     if ( usuario == "admin" & senha =="admin"){
+          window.location = "../../../dashboard-admin/dashboard.html"
+          done = 1;
+     }
+
+     if(done == 0){
+          alert('Dados incorretos, tente novamente');
+     }
+
+
+}
+
+
+
