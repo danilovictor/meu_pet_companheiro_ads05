d8a8aa9ef2d2514c1f6f1cff01eace4e5d7280d5
Danilo Victor <danilo-almeida12@hotmail.com>
2021-11-13 20:59:22 -0300

    loginadm

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,28 +2,34 @@ let exibirModal = document.getElementById('modaljs');
 
 exibirModal.addEventListener('click',mostrarAlert);
 
+ const generateId = ()=>{
+      Math.random()
+ }
+
+
+
 function mostrarAlert(){
-     swal( " Seu pedido de adoção está em análise " , " Em breve entramos em contato com você via whatsapp ou E-mail! " , " warning " );
-};
-
-
-function login(event){
-     event.preventDefault();
-     var usuario = document.querySelector('#emaillogin')[0].value;
-     usuario = usuario.toLowerCase();
-
-     var senha = document.querySelector('#senhalogin')[0].value;
-     senha = senha.toLowerCase();
-
-
-     if (usuario =="admin " && senha == "admin"){
-          alert('Dados corretos')
-          window.location = "adotar.html"
-     } 
-     else{
-          alert('Dados incorretos');
-     }
+     swal( ` Seu pedido de adoção está em análise ! Em breve entramos em contato com você via whatsapp ou E-mail! `);
 }
+
+
+function Login(event) {
+     event.preventDefault();
+     var usuario = document.getElementById('emaillogin').value;
+     usuario = usuario.toLowerCase();
+     var senha = document.getElementById('senhalogin').value;
+     senha = senha.toLowerCase();
+   
+     if (usuario == "bcf" && senha == "bcf") {
+       alert("dados corretos");
+       window.location = "index.html";
+     }else{
+       alert("Dados incorretos, tente novamente");
+     }
+   }
+
+
+
 
 
 
